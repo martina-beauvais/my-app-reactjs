12db99e5fc2d0c1af84999a9341937dd6abd8318
Martina Beauvais <marbeauvais17@gmail.com>
2022-08-25 11:51:28 -0300

    Segunda entega - Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import NavBar from './components/NavBar';
 import './app/style.css';
 
+
 function App() {
   return (
     <div>
       <NavBar/>
     </div>
-  )
+  );
 }
 
 export default App;
