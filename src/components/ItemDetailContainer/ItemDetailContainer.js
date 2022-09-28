fb281ab6697e73f8194dc04a080386376cc2b088
Martina Beauvais <marbeauvais17@gmail.com>
2022-09-28 16:13:33 -0300

    Item Collection I

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ItemDetail from './ItemDetail.js'
-import { getItemById } from '../../app/firebase-api.js';
+import { getItemById } from '../../app/firebase-api';
 
 const ItemDetailContainer = () => {
     const {itemId} = useParams()
